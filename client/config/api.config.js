bfcde2e32f08bd06a25a877cb9f26c1f30764ea8
lolongwell <lolong.well@qq.com>
2017-12-01 22:31:40 +0800

    修改域名前缀

--- client/config/api.config.js
+++ client/config/api.config.js
@@ -1,3 +1,3 @@
 const isPro = Object.is(process.env.NODE_ENV, 'production')
 
-module.exports = {baseUrl: isPro ? 'http://lolong.fengyun.com/api/' : 'api/'}
+module.exports = {baseUrl: isPro ? 'http://fengyun.lolong.xyz/api/' : 'api/'}
